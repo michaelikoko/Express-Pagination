b99f815cbce51f9c4fee38163d9dea384038d89f
Michael Ikoko <michaelikoko.o@gmail.com>
2024-06-25 16:35:05 +0100

    edited movies.js controller

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Express Pagination
-Server-side pagination in Express JS.
+# Express.js Server-Side Pagination Example
+
+This repository demonstrates server-side pagination in an Express.js application with MongoDB. It includes two implementations of pagination:
+
+1. **Basic Pagination**: Custom implementation without external libraries.
+2. **Library-Based Pagination**: Implementation using the `mongoose-paginate-v2` library.
+
+Refer to the [tutorial](https:google.com) for detailed instructions on setting up, running, and testing the application.
+
+---
+
+Happy coding!
--- final/controllers/movies.js
+++ final/controllers/movies.js
@@ -2,33 +2,29 @@ const Movie = require("../models/Movie");
 const { PaginationParameters } = require("mongoose-paginate-v2");
 
 async function getMoviesCustom(req, res) {
-  let query = Movie.find();
-
-  const currentPage = parseInt(req.query.page, 10) || 1;
+  const page = parseInt(req.query.page, 10) || 1;
   const limit = parseInt(req.query.limit, 10) || 10;
-  const offset = (currentPage - 1) * limit;
+  const offset = (page - 1) * limit;
 
-  query = query.skip(offset).limit(limit);
-  const movies = await query.exec();
-
-  const totalItems = await Movie.estimatedDocumentCount();
+  const movies = await Movie.find().skip(offset).limit(limit).exec();
+  const totalItems = await Movie.countDocuments({});
   const totalPages = Math.ceil(totalItems / limit);
 
-  return res.status(200).json({ totalItems, currentPage, totalPages, movies });
+  return res.status(200).json({ totalItems, page, totalPages, movies });
 }
 
 function getMoviesLibrary(req, res) {
   const { page, limit } = req.query;
 
   const options = {
-    page: parseInt(page, 10), 
+    page: parseInt(page, 10),
     limit: parseInt(limit, 10),
   };
 
   const movies = Movie.paginate({}, options).then((result) => {
     return res.status(200).json({
       totalItems: result.totalDocs,
-      currentPage: result.page,
+      page: result.page,
       totalPages: result.totalPages,
       movies: result.docs,
     });
@@ -38,7 +34,7 @@ function getMoviesLibrary(req, res) {
   Movie.paginate(...new PaginationParameters(req).get()).then((result) => {
     return res.status(200).json({
       totalItems: result.totalDocs,
-      currentPage: result.page,
+      page: result.page,
       totalPages: result.totalPages,
       movies: result.docs,
     });
